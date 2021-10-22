fb62d759ba6aaec5b7be7a864af9670376b86c68
DENOYEL Luc <denoyel.l@sfeir.com>
2021-10-22 14:48:59 +0200

    Deposit 210.2 on an account

--- src/test/java/fr/sg/business/UserCaseTest.java
+++ src/test/java/fr/sg/business/UserCaseTest.java
@@ -1,18 +1,17 @@
 package fr.sg.business;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UserCaseTest {
 
     Account account;
 
     @Test
-    public void should_create_a_account_with_balance_of_zero(){
+    public void should_create_a_account_with_balance_of_zero() {
         //Given
         Balance balance = new Balance();
 
@@ -24,7 +23,7 @@ public class UserCaseTest {
     }
 
     @Test
-    public void should_depose_100_on_an_empty_account(){
+    public void should_depose_100_on_an_account() {
         //Given
         Balance balance = new Balance();
         account = new Account(balance);
@@ -36,5 +35,20 @@ public class UserCaseTest {
 
         //Then
         assertEquals(account.getBalance().value, new BigDecimal(100));
+    }
+
+    @Test
+    public void should_depose_210_2_on_an_account() {
+        //Given
+        Balance balance = new Balance();
+        account = new Account(balance);
+
+        Amount deposit = new Amount(new BigDecimal("210.2"));
+
+        //When
+        account.deposit(deposit);
+
+        //Then
+        assertEquals(account.getBalance().value, new BigDecimal("210.2"));
     }
 }
