42bebd10e1cac3d276c8169139a368aafc538a20
DENOYEL Luc <denoyel.l@sfeir.com>
2021-10-22 17:30:50 +0200

    Revert unwanted change to OperationType

--- src/main/java/fr/sg/business/OperationType.java
+++ src/main/java/fr/sg/business/OperationType.java
@@ -1,11 +1,7 @@
 package fr.sg.business;
 
 public enum OperationType {
-    DEPOSIT() {
-        public Amount apply(Amount balance, Amount amount) {
-            return new Amount(balance.value.add(amount.value));
-        }
-    },
+    DEPOSIT,
     WITHDRAW;
 
 
